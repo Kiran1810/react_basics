882b3e4d451aca1093298470c3904dce3539b426
Kiran1810 <kirangill1810@gmail.com>
2023-10-23 15:38:51 +0530

    created the currency converter

---
+++ currencyConverter/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState,useEffect } from 'react'
+
+import './App.css'
+
+
+function App() {
+  const [state, setState] = useState()
+
+ 
+
+  
+  return (
+    <>
+  <h1 className='bg-orange-600'>currencyConverter</h1>
+  </>
+  )
+  }
+
+export default App
